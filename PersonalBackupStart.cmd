2d26338fad6264b2e1a1771b27ea79cb5c851732
Jurkis <atvejis@gmail.com>
2017-10-30 14:16:49 +0200

    Naujas CMD failas

---
+++ PersonalBackupStart.cmd
@@ -0,0 +1,3 @@
+@echo off
+   echo Start Personal Backup from USB flash drive
+   start \PortableApps\PersonalBackup\PersBackup.exe /ini:\PortableApps\PersonalBackup\pb.ini /taskdir:\PortableApps\PersonalBackup\darbai\ /logdir:%%progpath%%\logs
